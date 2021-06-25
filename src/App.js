284a0c434cbd8f4b54818ccc43b9e0e493059a17
Richa <18tiwariricha@gmail.com>
2021-06-25 11:42:29 +0530

    done

---
+++ src/App.js
@@ -0,0 +1,24 @@
+
+import './App.css';
+import Section1 from './components/Section1/section1.js'
+import Section2 from './components/Section2/section2';
+import Welcome from './components/Popup2/Popup2';  
+import Section3 from './components/Section3/Section3';
+import Section4 from './components/Section4/Section4';
+import Section5 from './components/Section5/Section5';
+import Section6 from './components/Section6/Section6';
+function App() {
+  return (
+    <div>
+      <Section1/>
+      <Section2/>
+        {/* <Welcome/>  */}
+       <Section3/>
+       <Section4/>
+       <Section5/>
+       <Section6/>
+    </div>
+  );
+}
+
+export default App;
