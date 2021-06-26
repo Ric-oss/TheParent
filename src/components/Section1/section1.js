8885fe9885cf49ea70c48ff6cc3cb4ab3584d557
Richa <18tiwariricha@gmail.com>
2021-06-26 14:45:08 +0530

    done

--- src/components/Section1/section1.js
+++ src/components/Section1/section1.js
@@ -14,6 +14,7 @@ function Section1(){
 
   return(
       <div className="section1">
+        <div className="container">
         <div className="header__part">
           <div>
         <img src={Subtract} className="Img"/>
@@ -35,6 +36,7 @@ function Section1(){
            </div>
            {welcome?<Welcome setPopup={setPopup} setWelcome={setWelcome}/>:""}
             {popup?<Popup setPopup={setPopup} setWelcome={setWelcome} />:""}
+            </div>
       </div>
   )
 
